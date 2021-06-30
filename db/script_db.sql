0cd33987c5c13a124508aa797013eb0fda646fa7
danwhat <daanwhat@gmail.com>
2021-06-30 18:04:41 -0300

    Fixed decimal at sql file

--- db/script_db.sql
+++ db/script_db.sql
@@ -12,10 +12,10 @@ id int auto_increment primary key,
 currency_id int not null,
 frequency int not null,
 reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-open_value decimal(9,8) not null,
-low_value decimal(9,8) not null,
-high_value decimal(9,8) not null,
-close_value decimal(9,8) not null,
+open_value decimal(14,8) not null,
+low_value decimal(14,8) not null,
+high_value decimal(14,8) not null,
+close_value decimal(14,8) not null,
 foreign key (currency_id) references currencies(id)
 );
 
