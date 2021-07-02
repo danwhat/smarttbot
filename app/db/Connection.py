6e0de974b18fe9a443aca3955507c48a7c50b852
danwhat <daanwhat@gmail.com>
2021-07-02 13:39:49 -0300

    fix host connection name

--- app/db/Connection.py
+++ app/db/Connection.py
@@ -7,7 +7,7 @@ class MySqlConnection:
 
   def __init__(self):
     self.__con = mysql.connector.connect(
-        host="localhost",
+        host="db",
         user="root",
         password="root",
         database="smarttbot"
@@ -23,21 +23,3 @@ class MySqlConnection:
 
   def close(self):
     self.__con.close()
-
-
-
-class CandleDao:
-  
-  __db = None
-
-  def __init__(self):
-    self.__db = MySqlConnection()
-  
-  def addCandle(self, currencyId, frequency, openValue, closeValue, lowValue, highValue):
-    query = f"insert into candles(currency_id, frequency, open_value, close_value, low_value, high_value) values ({currencyId},{frequency},{openValue},{closeValue},{lowValue},{highValue});"
-    self.__db.query(query)
-
-  def getAll(self):
-    query = "select * from candles"
-    result = self.__db.query(query)
-    return result
